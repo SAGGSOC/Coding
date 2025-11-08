8078b747e0753d0a7cb47c966449e352a43be8c1
sagar shivani <sagsora@amazon.com>
2025-11-08 14:43:21 +0530

    Add Subsequence Program

---
+++ Subsequence.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+
+/**
+ *
+ *  [3, 1, 2]
+ *  {} Empty
+ *  3
+ *  1
+ *  2
+ *  3 1
+ *  1 2
+ *  3 2
+ *  3 1 2
+ *
+ *  Order has to maintained
+ *
+ *  This will be done using Recursion
+ *
+ *  take/ notTake Approach
+ *
+ *  3 _ 2
+ *
+ *  _ 1 2
+ *
+ *  [].add(arr[i]);
+ *  f(ind + 1, []); take
+ *  [].remove(arr[i]);
+ *  f(ind,[])
+ */
+public class Subsequence {
+
+    static void f(int ind, int arr[], ArrayList<Integer> arr1, int n){
+        if(ind >= n) {
+            System.out.println(arr1);
+            return;
+        }
+        arr1.add(arr[ind]);
+        f(ind + 1, arr, arr1, n);
+        arr1.remove(arr1.size() - 1);
+
+        f(ind + 1, arr, arr1, n);
+    }
+    public  static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Enter the array");
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        f(0, arr, arr1, n);
+    }
+}
